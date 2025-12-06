a5b33fcd8e559d13f856902e65a25d6f2b7a040d
Mat Tremblay <kruedog@gmail.com>
2025-12-06 01:15:09 -0500

    Update get_coach.php

--- webapi/get_coach.php
+++ webapi/get_coach.php
@@ -21,22 +21,36 @@ if (!is_array($coaches)) {
 }
 
 // Case-insensitive username lookup
-foreach ($coaches as $c) {
-    if (isset($c['Username']) && strcasecmp($c['Username'], $username) === 0) {
+// We expect parallel arrays: files[] and imageType[]
+if (!empty($_FILES['files']) && !empty($_POST['imageType'])) {
 
-        // ✔ normalize file paths (just filename → -> uploads/filename)
-        if (isset($c['Files']) && is_array($c['Files'])) {
-            foreach ($c['Files'] as $type => $path) {
-                if ($path) {
-                    $c['Files'][$type] = 'uploads/' . basename($path);
-                }
-            }
+    $fileArray = $_FILES['files'];
+    $typeArray = $_POST['imageType'];
+
+    foreach ($typeArray as $index => $type) {
+        if (!isset($fileArray['name'][$index])) continue;
+        if ($fileArray['error'][$index] !== UPLOAD_ERR_OK) continue;
+
+        $origName = $fileArray['name'][$index];
+        $tmpPath  = $fileArray['tmp_name'][$index];
+
+        $ext = strtolower(pathinfo($origName, PATHINFO_EXTENSION));
+        if (!$ext) $ext = 'jpg';
+
+        $safeUser = preg_replace('/[^a-z0-9_\-]+/i','_', $username);
+        $safeType = preg_replace('/[^a-z0-9_\-]+/i','_', $type);
+
+        $newFile = "{$safeUser}_{$safeType}_" . time() . ".{$ext}";
+
+        $targetDisk = $diskUploadPath . $newFile;
+        $publicPath = $publicPathBase . $newFile;
+
+        if (move_uploaded_file($tmpPath, $targetDisk)) {
+            $coaches[$i]['Files'][$type] = $publicPath;
         }
-
-        echo json_encode(['success'=>true,'coach'=>$c], JSON_UNESCAPED_SLASHES);
-        exit;
     }
 }
+
 
 // Not found
 echo json_encode(['success'=>false,'message'=>'Coach not found']);
